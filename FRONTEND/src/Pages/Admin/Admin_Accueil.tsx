ce71a76ef6961279874f7db1edccf077e31f5589
Sitraka Herisoa Rakotoarivony <srakotoa@c1r4s15.42antananarivo.mg>
2026-01-20 13:08:21 +0300

    Display the finished sites in the Admin page

--- FRONTEND/src/Pages/Admin/Admin_Accueil.tsx
+++ FRONTEND/src/Pages/Admin/Admin_Accueil.tsx
@@ -10,6 +10,8 @@ const Admin_Accueil = () => {
     const textRed = "text-[#A02020]";
 
     const LoadingSites = sites.filter(site => site.statut === 'En cours');
+    const LoadingSitesFinished = sites.filter(site => site.statut === 'Terminé');
+
     
     return (
         <div className="space-y-6 text-white">
@@ -35,6 +37,32 @@ const Admin_Accueil = () => {
                                 {site.typeTravail}
                             </span>
                             <span className="flex items-center gap-1 text-amber-400 text-sm">
+                                <FaClock /> {site.statut}
+                            </span>
+                        </div>
+                        
+                        <h3 className="text-xl font-semibold mb-2">{site.description}</h3>
+                        
+                        <div className="space-y-2 text-gray-300 text-sm">
+                            <div className="flex items-center gap-2">
+                                <FaMapMarkerAlt className="text-[#6090A0]" />
+                                {site.localisation}
+                            </div>
+                            <div className="flex items-center gap-2 font-mono text-white">
+                                <FaMoneyBillWave className="text-[#245c49]" />
+                                {new Intl.NumberFormat('fr-MG', { style: 'currency', currency: 'MGA' }).format(site.depenseTotal)}
+                            </div>
+                        </div>
+						<div className='flex justify-end underline hover:cursor-pointer' onClick={() => navigate(`/sites?id=${site.id}`)}>voir plus</div>
+                    </div>
+                ))}
+				{LoadingSitesFinished.map((site) => (
+                    <div key={site.id} className="bg-[#1a2332] rounded-xl p-6 border-l-4 border-[#208060] shadow-xl hover:shadow-2xl transition-all">
+                        <div className="flex justify-between items-start mb-4">
+                            <span className="bg-[#208060]/20 text-[#409090] text-xs font-bold px-2 py-1 rounded uppercase">
+                                {site.typeTravail}
+                            </span>
+                            <span className="flex items-center gap-1 text-green-400 text-sm">
                                 <FaClock /> {site.statut}
                             </span>
                         </div>
